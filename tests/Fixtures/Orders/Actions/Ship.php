729b8db524fe9fe6712f03b3815ede8f316a105f
Justin Seliga <justinsel@zillowgroup.com>
2026-02-06 22:56:57 -0500

    wip

--- tests/Fixtures/Orders/Actions/Ship.php
+++ tests/Fixtures/Orders/Actions/Ship.php
@@ -10,6 +10,8 @@ use Support\Actions\Contracts\Action;
 final class Ship implements Action
 {
     use AsAction;
+    // TODO: Action catches ShouldQueue & Dispatachable prevention
+    // TODO: AsAction does not, figure out why
 
     public readonly string $input;
 
